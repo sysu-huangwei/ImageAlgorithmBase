c2a6b41078ff1a37a52d6294b454505a53fc2af9
黄炜 <hw1@meitu.com>
2018-11-01 10:28:42 +0800

    添加一个ios demo

---
+++ ImageAlgorithmBase/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by HW on 2018/11/1.
+//  Copyright © 2018 meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageAlgorithmBase/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by HW on 2018/11/1.
+//  Copyright © 2018 meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
